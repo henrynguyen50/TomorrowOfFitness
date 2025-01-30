47dafae7c33681448099d5ed70be26dec4625076
henrynguyen50 <henryhuynguyen17@gmail.com>
2025-01-29 22:35:27 -0600

    Finished FAQ with Mechanical tension talk and updated workout planner, still need new changes

--- website/views.py
+++ website/views.py
@@ -21,12 +21,26 @@ def faq():
 
     return render_template("FAQ.html", user=current_user)
 
-
-@views.route('/WorkoutPlanner')
+@views.route('/WorkoutPlanner', methods=['GET', 'POST'])
 @login_required
 def workout():
+    if request.method == 'POST':
+        full_body_note = request.form.get('full_body_note')
+        upper_lower_note = request.form.get('upper_lower_note')
+
+        if full_body_note and len(full_body_note) > 1:
+            new_note = Note(data=f"Full Body: {full_body_note}", user_id=current_user.id)
+            db.session.add(new_note)
+
+        if upper_lower_note and len(upper_lower_note) > 1:
+            new_note = Note(data=f"Upper Lower: {upper_lower_note}", user_id=current_user.id)
+            db.session.add(new_note)
+
+        db.session.commit()
+        flash('Workout added!', category='success')
 
     return render_template("WorkoutPlanner.html", user=current_user)
+
 
 @views.route('/brainrot')
 @login_required
@@ -34,4 +48,14 @@ def brainrot():
 
     return render_template("brainrot.html", user=current_user)
 
+@views.route('/delete-note', methods=['POST'])
+def delete_note():  
+    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
 
+    return jsonify({})
